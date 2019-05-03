339918626a062aced8b8a16f55a59f3d70a52ba7
kanjamad bosel <kanjamadpor@gmail.com>
2019-05-02 19:45:25 -0700

    sigup fix unique index on the name to be username

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -35,8 +35,8 @@ router.post('/signup', (req, res) => {
     const errors = [];
     
     // Validate Form Data
-    if (!req.body.name) {
-    errors.push({message: 'Please enter your name'});
+    if (!req.body.username) {
+    errors.push({message: 'Please enter your username'});
     }
 
     if (!req.body.currentCity) {
@@ -70,7 +70,7 @@ router.post('/signup', (req, res) => {
 
         // Create an object to hold the new user information (with hashed password, not original password)
         const newUser = {
-        name: req.body.name,
+        username: req.body.username,
         currentCity: req.body.currentCity,
         email: req.body.email,
         password: hash,
