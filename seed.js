78aacebf03c6505b83296e81f2ef14cfe357c6b4
kanjamad bosel <kanjamadpor@gmail.com>
2019-05-02 23:58:47 -0700

    add Post seed

--- seed.js
+++ seed.js
@@ -209,3 +209,51 @@ const userData = [
         process.exit();
         });
     });
+
+
+
+
+// ############################ sprint 2 ############################################
+
+
+
+    let posts = [{
+        title: "Trip to London",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",   
+    }, 
+    {
+        title: "Trip to Thailand",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",   
+    }, 
+    {
+        title: "Trip to San Francisco",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",   
+    },]
+    
+    db.User.remove({}, (err, Users) => {
+        db.Post.remove({}, (err, posts) => {});
+        db.Post.create(posts, (err, savedPosts) => {
+            if (err) {
+                return console.log(err);
+            } else {
+                console.log('post saved successfully');
+                db.User.create(user, (err, savedUser) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    savedPosts.forEach(Post => {
+                        savedUser.posts.push(Post);
+                    });
+                    // EMBEDDED
+                    savedUser.save((err, savedUser) => {
+                        if (err) {
+                            return console.log(err);
+                        } else {
+                            console.log('User is ', savedUser);
+                        }
+                    });
+                });
+            }
+        });
+    });
+    
